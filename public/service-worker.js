dce5ceb348b8ce6100eb51b9db53cc7c128c0acf
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-06-14 13:32:30 -0500

    Added db.js to index.html, fixed service worker issue

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
   "/db.js",
   "/index.html",
   "/index.js",
-  "/style.css",
+  "/styles.css",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
